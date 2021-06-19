168eabb7228dccabea50d3767aa41b5b95e1ea89
Yashsahu7895 <57767430+Yashsahu7895@users.noreply.github.com>
2021-06-19 11:19:29 +0530

    Api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+import axios from "axios";
+
+import "./App.css";
+const App = () => {
+  const [user, setUser] = useState(null);
+  const [pageNo, setPageNo] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
+
+  const gotoNext = () => {
+    const newPageNo = pageNo + 1;
+    setPageNo(newPageNo);
+  };
+  const gotoPrev = () => {
+    const newPageNo = pageNo - 1;
+    setPageNo(newPageNo);
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://reqres.in/api/users?page=" + pageNo)
+      .then((res) => {
+        console.log(res.data);
+        setUser(res.data.data);
+        setTotalPage(res.data.total_pages);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .then(() => {
+        console.log("Request Completed");
+      });
+  }, [pageNo]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Hello World</h2>
+      <button onClick={gotoPrev} disabled={pageNo === 1}>
+        Prev
+      </button>
+      <button onClick={gotoNext} disabled={pageNo === totalPage}>
+        Next
+      </button>
+      <h3> Page: {pageNo}</h3>
+      {user === null ? (
+        <h3>Loading....</h3>
+      ) : (
+        <div>
+          {user.map((item) => {
+            return (
+              <div className="userWrapper" key={item.id}>
+                <h3>
+                  {item.first_name} {item.last_name}
+                </h3>
+                <p>{item.email}</p>
+                <img src={item.avatar} alt="" />
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
-}
-
+};
 export default App;
